f1fe3209d7fc9338bb8a15fb38fd1158539f6148
Amit <amitdey1350@gmail.com>
2025-10-05 22:59:04 +0530

    ss

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,21 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Momentmm.xyz</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/momentmm',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.gg/momentmm',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/momentmm/docs',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Momentmm.xyz - Official Documentation',
+  },
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – Momentmm.xyz'
+    }
   },
 }
 
